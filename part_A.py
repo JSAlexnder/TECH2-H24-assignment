339308850f7be2e434b36489271dd0dd059a4c6e
Alexander Johannson Skjelbred <alexander.skjelbred@student.nhh.no>
2024-10-05 14:36:45 +0200

    Solution to partA and partB / AJS

--- part_A.py
+++ part_A.py
@@ -1,35 +1,64 @@
 """
-TECH2 mandatory assignment - Part A
-
-Write the implementation of part A of the exercise below.
+TECH2 mandatory assignment - Part A / AJS
 """
+import numpy as np
+
+from math import sqrt
+
+# Part A1
 
 def std_loops(x):
-    """
-    Compute standard deviation of x using loops.
+    # Create a function using for loops to find the standard deviation of a list x
+   
+    n = 0 # Create an empty value for N
+    ts = 0 # Create an empty value for total sum
+    tss = 0 # Create an empty value for total sum of squares
 
-    Parameters
-    ----------
-    x: Sequence of numbers
+    #Find N, total sum (ts) and total sum of squares (tss) for list x
+    for i in x:
+        n += 1
+        ts += i
+        tss += i**2
 
-    Returns
-    -------
-    sd : float
-        Standard deviation of the list of numbers.
-    """
+    #Find the mean (m) and the mean of squares (ms)
+    mean = ts/n
+    meansquares = tss/n
+
+    #Find variance s2 and standard deviation s
+    s2 = meansquares - mean**2
+    s = sqrt(s2)
+
+    return s
+
+num_lst = [1, 2, 3, 4, 5]
+sd_loops = std_loops(num_lst)
+print(f"Standard Deviation using loops: {sd_loops}")
+
+# Part A2
 
 def std_builtin(x):
-    """
-    Compute standard deviation of x using the built-in functions sum()
-    and len().
+  # Create a function using built in functions to find the standard deviation of a list x
+    n = len(x) # Find the lenght (n()) of the list x
+    ts = sum(x) # Find the total sum (ts) of the list x
+    tss =  sum(i**2 for i in x) # Find the total sum of squares (tss) of the list x
 
-    Parameters
-    ----------
-    x: Sequence of numbers
+    #Find the mean (m) and the mean of squares (ms)
+    mean = ts/n
+    meansquares = tss/n
 
-    Returns
-    -------
-    sd : float
-        Standard deviation of the list of numbers.
-    """
-    
+    #Find variance (s2) and standard deviation (s)
+    s2 = meansquares - mean**2
+    s = sqrt(s2)
+
+    #Function should return standard deviation s of list x
+    return s
+
+print(f"Standard Deviation using built in functions: {sd_loops}")
+
+# Part A3
+ 
+# Find the standard deviation of list x, using NumPy
+s = np.std(num_lst)
+print("Standard Deviation Using NumPy:", s)
+
+
